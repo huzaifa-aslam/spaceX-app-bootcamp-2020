e3f8ab9008147993721c5d2a4da39871d815c687
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-09-15 00:39:03 +0500

    files updated

--- src/components/MissionInfo/index.tsx
+++ src/components/MissionInfo/index.tsx
@@ -7,13 +7,19 @@ import {useLaunchMissionInfoQuery} from './../../generated/graphql'
 export const MissionInfo = ({userName}:any) => {
 
 
+    const [myid,setMyid]=useState(15)
+        // if(!userName){
+        //     return <h1>Loading...</h1>
+        // }
+        // !userName ? '<h3>Loading..</h3>': setMyid(userName)
 
-    const [myid,setMyid]=useState<any>(12)
-    // setMyid(userName)
+
+    console.log("userName",userName)
 
     const { data, loading, error } = useLaunchMissionInfoQuery({
            variables: {
-              id:myid.toString() // value for 'id'
+
+              id:userName.toString() // value for 'id'
            },
          });
 
